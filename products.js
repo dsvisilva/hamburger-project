7c439b9750e858ece341fc2fa34c0ff20cbc82bb
dsvisilva <sdavih06@gmail.com>
2025-10-06 16:27:28 -0300

    hamburger changes

--- products.js
+++ products.js
@@ -1,11 +1,12 @@
 const menuOptions = [
-{ name: 'X-Salada', price: 30, vegan: false, src: './assets/xsalada.png' },
 
 { name: 'X-Bacon', price: 34, vegan: false, src: './assets/xbacon.png' },
 
 { name: 'X-Bacon Egg', price: 39, vegan: false, src: './assets/bacon-egg.png' },
 
 { name: 'Monstruoso', price: 50, vegan: false, src: './assets/monstruoso.png' },
+
+{ name: 'X-Salada', price: 30, vegan: false, src: './assets/xsalada.jpeg' },
 
 { name: 'Big Vegano', price: 55, vegan: true, src: './assets/xvegan.png' },
 
--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+const buttonShowAll = document.getElementById("show");
+const buttonMap = document.getElementById("map");
+const sumAll = document.getElementById("sum");
+const filter = document.getElementById("filter");
 
+const list = document.getElementById("menu");
 
-const mostAll = document.getElementById('most');
-const sumAll = document.getElementById('sum');
-const filter = document.getElementById('filter');
-const map = document.getElementById('map');
+let myLi = "";
 
-const list = document.getElementById('menu');
-
-let myLi = ''
-
-menuOptions.forEach((product) => {
-    myLi = myLi + `
+function showAll(productsArray) {
+  myLi = "";
+  productsArray.forEach((product) => {
+    myLi += `
         <li>
             <img src="${product.src}" alt="${product.name}">
             <p class = "hamburger">${product.name}</p>
-            <p class = "price">R$ ${product.price},00</p>
+            <p class = "price">R$ ${product.price}</p>
         </li>
-    `
+    `;
+  });
+  list.innerHTML = myLi;
+}
 
-})
+buttonShowAll.addEventListener("click", () => showAll(menuOptions));
+
+function discountAll() {
+  const newPrices = menuOptions.map((product) => ({
+    ...product,
+    price: product.price * 0.9,
+  }));
+
+  showAll(newPrices);
+}
+
+buttonMap.addEventListener("click", discountAll);
