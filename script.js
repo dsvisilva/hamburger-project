2e3eae75e2188fbac57300424e510293a0e1d199
dsvisilva <sdavih06@gmail.com>
2025-10-06 12:21:46 -0300

    hamburger changes

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-const menuOptions = [
-{ name: 'X-Salada', price: 30, vegan: false, src: './assets/xsalada.png' },
 
-{ name: 'X-Bacon', price: 34, vegan: false, src: './assets/xbacon.png' },
 
-{ name: 'X-Bacon Egg', price: 39, vegan: false, src: './assets/bacon-egg.png' },
-
-{ name: 'Monstruoso', price: 50, vegan: false, src: './assets/monstruoso.png' },
-
-{ name: 'Big Vegano', price: 55, vegan: true, src: './assets/xvegan.png' },
-
-{ name: 'X-Vegan', price: 45, vegan: true, src: './assets/monstruoso-vegan.png' },
-]
-
-const mostAll = document.getElementById('most-all');
-const sumAll = document.getElementById('sum-all');
+const mostAll = document.getElementById('most');
+const sumAll = document.getElementById('sum');
 const filter = document.getElementById('filter');
 const map = document.getElementById('map');
 
-map.addEventListener('click', () => {
-    console.log('funcionou')
-});
+const list = document.getElementById('menu');
 
-mostAll.addEventListener('click', () => {
-    console.log('aeeee')
-});
+let myLi = ''
 
-sumAll.addEventListener('click', () => {
-    console.log('foi')
-});
+menuOptions.forEach((product) => {
+    myLi = myLi + `
+        <li>
+            <img src="${product.src}" alt="${product.name}">
+            <p class = "hamburger">${product.name}</p>
+            <p class = "price">R$ ${product.price},00</p>
+        </li>
+    `
 
-filter.addEventListener('click', () => {
-    console.log('deu boa')
-});
+})
