1117e161b5f1012eb8e29134811cf560d36a38d2
dsvisilva <sdavih06@gmail.com>
2025-10-07 10:44:33 -0300

    haburger finish

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const buttonShowAll = document.getElementById("show");
 const buttonMap = document.getElementById("map");
 const sumAll = document.getElementById("sum");
-const filter = document.getElementById("filter");
+const btnFilter = document.getElementById("filter");
 
 const list = document.getElementById("menu");
+
+function formatCurrency(value) {
+  const newValue = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return newValue;
+}
 
 let myLi = "";
 
@@ -14,7 +22,7 @@ function showAll(productsArray) {
         <li>
             <img src="${product.src}" alt="${product.name}">
             <p class = "hamburger">${product.name}</p>
-            <p class = "price">R$ ${product.price}</p>
+            <p class = "price">${formatCurrency (product.price)}</p>
         </li>
     `;
   });
@@ -34,12 +42,31 @@ function sumAllProducts() {
   const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0);
   list.innerHTML = `
         <li>        
-            <p>O valor total é: R$ ${totalValue}</p>
+            <p>O valor total é: ${formatCurrency (totalValue)}</p>
         </li>
     `;
+}
+
+function onlyVegan() {
+  const onlyVegan = menuOptions.filter((product) => product.vegan)
+  showAll(onlyVegan)
+}
+
+let showingVegan = false
+
+function toggleVegan() {
+  showingVegan = !showingVegan
+
+  if (showingVegan) {
+    onlyVegan()
+    btnFilter.textContent = "Todos"
+  } else {
+    showAll(menuOptions)
+    btnFilter.textContent = "Filtrar"
+  }
 }
 
 buttonShowAll.addEventListener("click", () => showAll(menuOptions));
 buttonMap.addEventListener("click", discountAll);
 sumAll.addEventListener("click", sumAllProducts);
-filter.addEventListener("click", )
+btnFilter.addEventListener("click", toggleVegan)
